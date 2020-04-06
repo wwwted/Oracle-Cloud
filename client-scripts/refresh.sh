b408447a787ad521e8dece4211a807fdda3e60f0
wwwted <ted.wennmark@gmail.com>
2020-04-06 19:43:32 +0200

    new client scripts

---
+++ client-scripts/refresh.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+source $HOME/setenv
+
+[ ! -d $MYSQL_HOME ] && echo "Could not find MySQL dir" && exit
+[ ! -f $MYSQL_HOME/mysql/bin/mysql ] && echo "Could not find mysql binaries ($MYSQL_HOME/mysql/bin/mysql)" && exit
+[ ! -f $MYSQL_HOME/my.cnf ] && echo "Could not find my.cnf" && exit
+
+# Clean-up
+mysqladmin -uroot shutdown
+rm -fr $MYSQL_HOME/mysqldata
+
+mkdir $MYSQL_HOME/mysqldata
+mysqld --initialize-insecure --datadir=$MYSQL_HOME/mysqldata --basedir=$MYSQL_HOME/mysql
+mysqld_safe --defaults-file=$MYSQL_HOME/my.cnf --ledir=$MYSQL_HOME/mysql/bin &
+
+sleep 5
+
+mysql -uroot -e"SET SQL_LOG_BIN=0; CREATE USER 'ted'@'%' IDENTIFIED BY 'ted'; GRANT ALL ON *.* TO 'ted'@'%' WITH GRANT OPTION";
+pgrep mysql
+mysql -uted -pted -e"status"
+mysql -uted -pted -e "select @@hostname, @@global.gtid_executed"
+
